e7032130928c739cbbd45c28273ad308c3929abc
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-10 12:42:44 +0530

    added Problem Statement & empty project with welcome message

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Flip Coin Problem\n");
+        }
+    }
+}
