6f12188d3814b2929e94ae80206b581c0469cbdd
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-10 13:56:55 +0530

    added uc2: logic to calculate heads and tails

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -8,7 +8,9 @@ namespace FlipCoin
         {
             Console.WriteLine("Welcome to the Flip Coin Problem\n");
             int TotalFlip = TakeInput();
-            Console.WriteLine("\nNumber of times to Flip is: " + TotalFlip);
+            int TotalHead = CoinCheck(TotalFlip);
+            int TotalTail = TotalFlip - TotalHead;
+            Console.WriteLine("\nH: " + TotalHead + " T: " + TotalTail);
         }
 
         public static int TakeInput()
@@ -16,6 +18,20 @@ namespace FlipCoin
             Console.Write("Enter how many times to Flip: ");
             string Num = Console.ReadLine();
             return Convert.ToInt32(Num);
+        }
+
+        public static int CoinCheck(int TotalFlip)
+        {
+            Random random = new Random();
+            int HeadNum = 0;
+            while (TotalFlip > 0)
+            {
+                int CoinFlip = random.Next(0, 2);
+                if (CoinFlip == 0)
+                    HeadNum++;
+                TotalFlip--;
+            }
+            return HeadNum;
         }
     }
 }
