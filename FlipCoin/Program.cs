c188fe6335d48928d77603fe68e2b602a1459b16
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-10 13:09:05 +0530

    added uc1: Take numer of times to flip a Coin as Input

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -7,6 +7,15 @@ namespace FlipCoin
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Flip Coin Problem\n");
+            int TotalFlip = TakeInput();
+            Console.WriteLine("\nNumber of times to Flip is: " + TotalFlip);
+        }
+
+        public static int TakeInput()
+        {
+            Console.Write("Enter how many times to Flip: ");
+            string Num = Console.ReadLine();
+            return Convert.ToInt32(Num);
         }
     }
 }
